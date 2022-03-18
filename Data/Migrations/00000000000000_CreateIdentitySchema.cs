1bbfb0555cfe3a8090622f5bbbc6d63c5ca7184e
eekohardianto <eekohardianto@gmail.com>
2022-03-18 17:03:10 +0700

    menambahkan database Postgres

--- Data/Migrations/00000000000000_CreateIdentitySchema.cs
+++ Data/Migrations/00000000000000_CreateIdentitySchema.cs
@@ -30,7 +30,7 @@ namespace ProjectJoker.Data.Migrations
                     NormalizedUserName = table.Column<string>(maxLength: 256, nullable: true),
                     Email = table.Column<string>(maxLength: 256, nullable: true),
                     NormalizedEmail = table.Column<string>(maxLength: 256, nullable: true),
-                    EmailConfirmed = table.Column<bool>(nullable: false),
+                    EmailConfirmed = table.Column<int>(nullable: false),
                     PasswordHash = table.Column<string>(nullable: true),
                     SecurityStamp = table.Column<string>(nullable: true),
                     ConcurrencyStamp = table.Column<string>(nullable: true),
